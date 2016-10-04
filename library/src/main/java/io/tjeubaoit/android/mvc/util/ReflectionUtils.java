4e57b605d0ef4759860e4a2cb690f27cf04ccd6f
tjeubaoit <anhtn.bk@gmail.com>
2016-10-05 01:19:14 +0700

    Support instant run

--- library/src/main/java/io/tjeubaoit/android/mvc/util/ReflectionUtils.java
+++ library/src/main/java/io/tjeubaoit/android/mvc/util/ReflectionUtils.java
@@ -2,10 +2,13 @@ package io.tjeubaoit.android.mvc.util;
 
 import android.content.Context;
 
+import java.io.File;
 import java.io.IOException;
 import java.lang.annotation.Annotation;
+import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import dalvik.system.DexFile;
@@ -36,20 +39,40 @@ public class ReflectionUtils {
             throws IOException, ClassNotFoundException {
 
         Set<Class<?>> classes = new HashSet<>();
-        DexFile dex = new DexFile(context.getApplicationInfo().sourceDir);
+        DexFile baseDex = new DexFile(context.getApplicationInfo().sourceDir);
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-        Enumeration<String> entries = dex.entries();
 
-        while (entries.hasMoreElements()) {
-            String entry = entries.nextElement();
-            for (String prefix : prefixes) {
-                if (entry.startsWith(prefix)) {
-                    classes.add(classLoader.loadClass(entry));
-                    break;
+        List<DexFile> dexFiles = new ArrayList<>();
+        dexFiles.add(baseDex);
+
+        // support instant run
+        File[] files = new File(getInstantRunOutputDir(context)).listFiles();
+        for (File file : files) {
+            if (file.isFile() && file.getName().matches(INSTANT_RUN_REGEX)) {
+                dexFiles.add(new DexFile(file.getAbsoluteFile()));
+            }
+        }
+
+        for (DexFile dex : dexFiles) {
+            Enumeration<String> entries = dex.entries();
+            while (entries.hasMoreElements()) {
+                String entry = entries.nextElement();
+                for (String prefix : prefixes) {
+                    if (entry.startsWith(prefix)) {
+                        classes.add(classLoader.loadClass(entry));
+                        break;
+                    }
                 }
             }
         }
 
         return classes;
+    }
+
+    static final String INSTANT_RUN_DIR = "/files/instant-run/dex";
+    static final String INSTANT_RUN_REGEX = "slice-slice_[0-9]+-classes.dex";
+
+    static String getInstantRunOutputDir(Context context) {
+        return context.getApplicationInfo().dataDir + INSTANT_RUN_DIR;
     }
 }
